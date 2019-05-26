6ec46bd412842e910a3e4ff689557210810b2a88
sera.prokop@gmail.com <sera.prokop@gmail.com>
2019-05-26 15:51:01 +0300

    main-catalog block finish

--- markup/components/main-catalog/data/data.js
+++ markup/components/main-catalog/data/data.js
@@ -1,1 +1,34 @@
-var data = {'main-catalog': {}};
+var data = {'main_catalog': {
+        title: "Каталог чая Матча",
+        list: [
+            {
+                img: "matcha.jpg",
+                title: "Матча царемониальный"
+            },
+            {
+                img: "matcha2.jpg",
+                title: "Матча премиальный"
+            },
+            {
+                img: "matcha3.jpg",
+                title: "Матча традиционный"
+            },
+            {
+                img: "drinks.jpg",
+                title: "Напитки"
+            },
+            {
+                img: "deserts.jpg",
+                title: "Десерты"
+            },
+            {
+                img: "accessories.jpg",
+                title: "Аксессуары"
+            },
+            {
+                img: "sets.jpg",
+                title: "Наборы"
+            },
+        ]
+    }
+};
