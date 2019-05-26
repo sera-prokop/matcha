9e31af8e66a0b16b1105a4f0a6a02e0b00ee31eb
sera.prokop@gmail.com <sera.prokop@gmail.com>
2019-05-26 19:01:54 +0300

    popular slider

--- markup/components/sliders/sliders.js
+++ markup/components/sliders/sliders.js
@@ -1,7 +1,7 @@
 import Swiper from 'swiper';
 
 /* eslint-disable no-unused-vars */
-let mySwiper = new Swiper('.swiper-container', {
+let mySwiper = new Swiper('.main-slider__container', {
     loop: true,
     lazy: true,
 
@@ -19,5 +19,22 @@ let mySwiper = new Swiper('.swiper-container', {
     //     delay: 2500,
     //     disableOnInteraction: false,
     // },
+});
+
+let SliderCard = new Swiper('.slider-card__container', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    lazy: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
 });
 /* eslint-enable no-unused-vars */
