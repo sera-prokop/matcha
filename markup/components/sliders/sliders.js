984b6a8248c5a1d37ee67109affeb92299b39fed
Sergey <sera.prokop@gmail.com>
2019-05-31 19:01:01 +0300

    product page

--- markup/components/sliders/sliders.js
+++ markup/components/sliders/sliders.js
@@ -43,4 +43,45 @@ let SliderCard = new Swiper('.slider-card__container', {
         }
     }
 });
+
+
+// Product slider
+
+let galleryThumbs = new Swiper('.gallery-thumbs', {
+    spaceBetween: 25,
+    slidesPerView: 4,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+    centerInsufficientSlides: true,
+    slideToClickedSlide: true
+});
+
+let galleryTop = new Swiper('.gallery-top', {
+    // spaceBetween: 10,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    thumbs: {
+        swiper: galleryThumbs
+    },
+    on: {
+        slideChange: function () {
+            let activeIndex = this.activeIndex + 1;
+
+            let activeSlide = document.querySelector(`.gallery-thumbs .swiper-slide:nth-child(${activeIndex})`);
+            let nextSlide = document.querySelector(`.gallery-thumbs .swiper-slide:nth-child(${activeIndex + 1})`);
+            let prevSlide = document.querySelector(`.gallery-thumbs .swiper-slide:nth-child(${activeIndex - 1})`);
+
+            if (nextSlide && !nextSlide.classList.contains('swiper-slide-visible')) {
+                this.thumbs.swiper.slideNext()
+            } else if (prevSlide && !prevSlide.classList.contains('swiper-slide-visible')) {
+                this.thumbs.swiper.slidePrev()
+            }
+
+        }
+    }
+});
+
+
 /* eslint-enable no-unused-vars */
